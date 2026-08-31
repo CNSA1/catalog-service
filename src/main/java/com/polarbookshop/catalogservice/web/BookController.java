4d639e5882cea5777c1b38413fb026fc3893de5b
vkhoma <16196348+HVV83@users.noreply.github.com>
2026-08-31 08:37:57 +0200

    Externalize configuration with properties

--- src/main/java/com/polarbookshop/catalogservice/web/BookController.java
+++ src/main/java/com/polarbookshop/catalogservice/web/BookController.java
@@ -24,6 +24,11 @@ public class BookController {
         this.bookService = bookService;
     }
 
+    @GetMapping
+    public Iterable<Book> get() {
+        return bookService.viewBookList();
+    }
+
     @GetMapping("{isbn}")
     public Book getByIsbn(@PathVariable String isbn) {
         return bookService.viewBookDetails(isbn);
